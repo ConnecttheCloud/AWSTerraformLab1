322b01987712a83faf73485061adfed745bd3c52
AddictedtoCloud <92588430+ConnecttheCloud@users.noreply.github.com>
2023-08-15 14:48:35 +0800

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-sudo yum -y update # update system
+
+#sudo yum -y update # update system
 sudo yum install -y httpd # install Apache server
 sudo systemctl start httpd # start Apache server
 sudo systemctl enable httpd # configure to restart Apache upon (re)boot
+
+sudo chown -R $USER:$USER /var/www/html
 
 sudo echo "<html>" > /var/www/html/index.html
 sudo echo "<body>" >> /var/www/html/index.html
 
 sudo echo "<h1>Public IP Address: $(curl -s http://ident.me)</h1>"  >> /var/www/html/index.html
-sudo echo "<h1>Hostname: $(hostname -f)</h1>"  >> /var/www/html/index.html
+sudo echo "<h1>EC2 hostname: $(hostname -f)</h1>"  >> /var/www/html/index.html
 sudo echo "<h1>Private IP Address: $(ip route get 1.2.3.4 | awk '{print $7}')</h1>"  >> /var/www/html/index.html
-#sudo echo "<p>This is the Web Server Demo with Auto Scaling Group and AWS App loadbalancer.</p>" >> /var/www/html/index.html
-sudo echo "<p>App Gatway and  VM scale sets backend pool loadbalancing demo.</p>" >> /var/www/html/index.html
+sudo echo "<p>This is the VMSS DEMO.</p>" >> /var/www/html/index.html
 
 sudo echo "</body>" >> /var/www/html/index.html
 sudo echo "</html>" >> /var/www/html/index.html
